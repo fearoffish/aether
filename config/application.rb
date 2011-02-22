6ba7a83d164d86a78aa5e796fd9e43f42eb15aea
Jamie van Dyke <jamie@fearoffish.com>
2011-02-22 14:46:53 +0000

    the default doctype is html5 now for haml, so this is no longer needed

--- config/application.rb
+++ config/application.rb
@@ -41,7 +41,5 @@ module Aether
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
-
-    Haml::Template.options[:format] = :html5
   end
 end
